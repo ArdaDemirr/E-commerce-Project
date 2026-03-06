0ecfbd0a1c1de57acd2b49b9c7407210331912ca
Arda Demir <ardademir4207@gmail.com>
2026-03-06 18:37:39 +0300

    fixed .env

--- backend/src/main/java/com/advanced/projectspring/ProjectspringApplication.java
+++ backend/src/main/java/com/advanced/projectspring/ProjectspringApplication.java
@@ -2,8 +2,10 @@ package com.advanced.projectspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:.env", ignoreResourceNotFound = true)
 public class ProjectspringApplication {
 
 	public static void main(String[] args) {
